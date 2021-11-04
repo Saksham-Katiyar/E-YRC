bb19a03272c54658a71d231c9e662c9520cdb92f
Chandu <chandramouli085@gmail.com>
2021-11-05 00:53:58 +0530

    task 1b mostly completed

--- Task1/task_1b_go_to_goal_obs/lib/toy_robot.ex
+++ Task1/task_1b_go_to_goal_obs/lib/toy_robot.ex
@@ -74,143 +74,185 @@ defmodule ToyRobot do
   Spawn a process and register it with name ':client_toyrobot' which is used by CLI Server to send an
   indication for the presence of obstacle ahead of robot's current position and facing.
   """
-  @directions_to_nums %{:north => 0, :south => 2, :east=> 1, :west=> 3}
+  @directions_to_the_opposite %{:north => 0, :south => 2, :east=> 1, :west=> 3}
   @directions_to_the_right %{north: :east, east: :south, south: :west, west: :north}
   @directions_to_the_left Enum.map(@directions_to_the_right, fn {from, to} -> {to, from} end)
-  def stop(%ToyRobot.Position{x: x, y: y, facing: facing} = robot, goal_x, goal_y, cli_proc_name) do
+  def stop(%ToyRobot.Position{x: _x, y: _y, facing: _facing} = robot, goal_x, goal_y, cli_proc_name) do
     self_pid=self()
-    pid=spawn(fn -> loop(self_pid) end)
+    _pid=spawn(fn -> loop(self_pid) end)
     Process.register(self_pid, :client_toyrobot)
     obs_map=[]
     index=0
     {:ok, obs_map}=make_obsmap(obs_map,index)
     traverse(robot,obs_map, goal_x, goal_y, cli_proc_name)
   end
-  def traverse(%ToyRobot.Position{x: x, y: y, facing: facing} = robot, obs_map, goal_x, goal_y, cli_proc_name) when x==goal_x and y==goal_y do
-    is_obstacle=send_robot_status(robot, cli_proc_name)
+  def traverse(%ToyRobot.Position{x: x, y: y, facing: _facing} = robot, _obs_map, goal_x, goal_y, cli_proc_name) when x==goal_x and y==goal_y do
     {:ok, robot}
   end
-  def traverse(%ToyRobot.Position{x: x, y: y, facing: facing} = robot, obs_map, goal_x, goal_y, cli_proc_name) do
+  def traverse(%ToyRobot.Position{x: x, y: y, facing: _facing} = robot, obs_map, goal_x, goal_y, cli_proc_name) do
+    is_obstacle=send_robot_status(robot,cli_proc_name)
+    obs_map=update_obs_map(robot, obs_map,is_obstacle)
     goal_index=@robot_map_y_atom_to_num[goal_y]*5+goal_x-6
     position_index=@robot_map_y_atom_to_num[y]*5+x-6
     shortest_path=[position_index]
     index=0
-    shortest_path=make_shortest_path(shortest_path,goal_index,index,obs_map)
-    {robot, obs_map}=move_on_shortest_path(robot,shortest_path, index, cli_proc_name, obs_map)
-    traverse(robot, obs_map, goal_x, goal_y, cli_proc_name)
+    goal_position_index=Enum.at(shortest_path, index)
+    shortest_path=make_shortest_path(shortest_path,goal_index,index,obs_map,goal_position_index)
+    # IO.puts Enum.at(shortest_path,0)
+    # IO.puts Enum.at(shortest_path,1)
+    # IO.puts Enum.at(shortest_path,2)
+    # IO.puts Enum.at(shortest_path,3)
+    # IO.puts Enum.at(shortest_path,4)
+    # IO.puts Enum.at(shortest_path,5)
+    # IO.puts Enum.at(shortest_path,6)
+    {robot,obs_map}=move_on_shortest_path(robot,shortest_path, index, cli_proc_name, obs_map, is_obstacle)
+    traverse(robot,obs_map, goal_x, goal_y, cli_proc_name)
   end
   def find_shortest_path(shortest_path,index,li) when index==0 do
+    li=List.insert_at(li, 0, Enum.at(shortest_path,index))
     li
   end
   def find_shortest_path(shortest_path,index,li) do
     li=List.insert_at(li, 0, Enum.at(shortest_path,index))
     find_shortest_path(shortest_path,trunc((index-1)/4),li)
   end
-  def make_shortest_path(shortest_path,goal_index,index,obs_map) when Enum.at(shortest_path, index)==goal_index do
+  def make_shortest_path(shortest_path,goal_index,index,_obs_map,goal_position_index) when goal_position_index==goal_index do
     li=[]
     find_shortest_path(shortest_path,index,li)
   end
-  def make_shortest_path(shortest_path,goal_index,index,obs_map) do
+  def make_shortest_path(shortest_path,goal_index,index,obs_map, _goal_position_index) do
     shortest_path=shortest_path++find_neighbour_indexes(Enum.at(shortest_path,index),obs_map)
-    make_shortest_path(shortest_path,goal_index,index+1,obs_map)
+    goal_position_index=Enum.at(shortest_path, index+1)
+    make_shortest_path(shortest_path,goal_index,index+1,obs_map, goal_position_index)
   end
   def find_neighbour_indexes(position_index,obs_map) do
-
+    x=rem(position_index,5)+1
+    y=trunc(position_index/5)+1
     neighbour_indexes=if position_index==-1 do
                         [-1,-1,-1,-1]
                       else
                         neighbour_indexes=[]
-                        neighbour_indexes=if position_index<20 and obs_map((@robot_map_y_atom_to_num[y])*5+x+14)!=1 do
-                                            List.insert_at(neighbour_dists, 0, position_index+5))
+                        neighbour_indexes=if position_index<20 and Enum.at(obs_map,(y*5+x+14))!=1 do
+                                            List.insert_at(neighbour_indexes, 0, position_index+5)
                                           else
-                                            List.insert_at(neighbour_dists, 0, -1)
+                                            List.insert_at(neighbour_indexes, 0, -1)
                                           end
-                        neighbour_indexes=if rem(position_index+1,5)!=0 and obs_map((@robot_map_y_atom_to_num[y])*4+x-5)!=1 do
-                                            List.insert_at(neighbour_dists, 1, position_index+1)
+                        neighbour_indexes=if rem(position_index+1,5)!=0 and Enum.at(obs_map,(y*4+x-5))!=1 do
+                                            List.insert_at(neighbour_indexes, 1, position_index+1)
                                           else
-                                            List.insert_at(neighbour_dists, 1, -1)
+                                            List.insert_at(neighbour_indexes, 1, -1)
                                           end
-                        neighbour_indexes=if position_index>4 and obs_map((@robot_map_y_atom_to_num[y])*5+x+9)!=1 do
-                                            List.insert_at(neighbour_dists, 2, position_index-5)
+                        neighbour_indexes=if position_index>4 and Enum.at(obs_map,(y*5+x+9))!=1 do
+                                            List.insert_at(neighbour_indexes, 2, position_index-5)
                                           else
-                                            List.insert_at(neighbour_dists, 2, -1)
+                                            List.insert_at(neighbour_indexes, 2, -1)
                                           end
-                        neighbour_indexes=if rem(position_index,5)!=0 and obs_map((@robot_map_y_atom_to_num[y])*4+x-6)!=1 do
-                                            List.insert_at(neighbour_dists, 3, position_index-1)
+                        neighbour_indexes=if rem(position_index,5)!=0 and Enum.at(obs_map,(y*4+x-6))!=1 do
+                                            List.insert_at(neighbour_indexes, 3, position_index-1)
                                           else
-                                            List.insert_at(neighbour_dists, 3, -1)
+                                            List.insert_at(neighbour_indexes, 3, -1)
                                           end
                         neighbour_indexes
                       end
     neighbour_indexes
   end
-  def move_on_shortest_path(%ToyRobot.Position{x: x, y: y, facing: facing} = robot, shortest_path, index, cli_proc_name, obs_map) when index==length(shortest_path)-1 do
-    robot
-  end
-  def move_on_shortest_path(%ToyRobot.Position{x: x, y: y, facing: facing} = robot, shortest_path, index, cli_proc_name, obs_map) do
-    is_obstacle=send_robot_status(robot,cli_proc_name)
-    obs_map=update_obs_map(robot, obs_map,is_obstacle)
-    robot=if is_obstacle==false do
-            {robot,obs_map,is_obstacle}=move_to_next_index(robot,Enum.at(shortest_path,index),Enum.at(shortest_path,index+1),cli_proc_name,obs_map)
-            robot=if is_obstacle==false
-                    move_on_shortest_path(robot,shortest_path, index+1, cli_proc_name, obs_map)
-                  end
-            robot
-          end
+  def move_on_shortest_path(%ToyRobot.Position{x: _x, y: _y, facing: _facing} = robot, shortest_path, index, _cli_proc_name, obs_map,_is_obstacle) when index==length(shortest_path)-1 do
     {robot,obs_map}
   end
-  def move_to_next_index(%ToyRobot.Position{x: x, y: y, facing: facing} = robot, index_0, index_1, cli_proc_name,obs_map) do
+  def move_on_shortest_path(%ToyRobot.Position{x: _x, y: _y, facing: _facing} = robot, shortest_path, index, cli_proc_name, obs_map, is_obstacle) do
+    {robot,obs_map,is_obstacle}=move_to_next_index(robot,Enum.at(shortest_path,index),Enum.at(shortest_path,index+1),cli_proc_name,obs_map)
+    {robot,obs_map}=if is_obstacle==false do
+                      move_on_shortest_path(robot,shortest_path, index+1, cli_proc_name, obs_map,is_obstacle)
+                    else
+                      {robot,obs_map}
+                    end
+
+    {robot,obs_map}
+  end
+  def move_to_next_index(%ToyRobot.Position{x: _x, y: _y, facing: facing} = robot, index_0, index_1, cli_proc_name,obs_map) do
     direction=cond do
                 index_1==index_0+5 -> :north
                 index_1==index_0+1 -> :east
                 index_1==index_0-5 -> :south
                 index_1==index_0-1 -> :west
               end
-    robot=cond do
-      facing==direction -> robot
-      @directions_to_the_right[facing]==direction -> right(robot)
-      @directions_to_the_left[facing]==direction -> left(robot)
-      true -> right(right(robot))
+
+    {robot, is_obstacle, obs_map}= if @directions_to_the_right[facing]==direction do
+      robot=right(robot)
+      is_obstacle=send_robot_status(robot,cli_proc_name)
+      obs_map=update_obs_map(robot, obs_map, is_obstacle)
+      {robot, is_obstacle, obs_map}
+    else
+      {robot, is_obstacle, obs_map}= if @directions_to_the_left[facing]==direction do
+        robot=left(robot)
+        is_obstacle=send_robot_status(robot,cli_proc_name)
+        obs_map=update_obs_map(robot, obs_map, is_obstacle)
+        {robot, is_obstacle, obs_map}
+      else
+        {robot, is_obstacle, obs_map}= if @directions_to_the_opposite[facing]==direction do
+          robot=right(robot)
+          _is_obstacle=send_robot_status(robot,cli_proc_name)
+          robot=right(robot)
+          is_obstacle=send_robot_status(robot,cli_proc_name)
+          obs_map=update_obs_map(robot, obs_map, is_obstacle)
+          {robot, is_obstacle, obs_map}
+        else
+          {robot, false, obs_map}
+        end
+      end
     end
-    is_obstacle=send_robot_status(robot,cli_proc_name)
-    obs_map=update_obs_map(robot, obs_map, is_obstacle)
     robot=if is_obstacle==false do
             move(robot)
+          else
+            robot
           end
     {is_obstacle, obs_map}= if is_obstacle==false do
                               is_obstacle=send_robot_status(robot,cli_proc_name)
                               obs_map=update_obs_map(robot, obs_map, is_obstacle)
                               {is_obstacle, obs_map}
+                            else
+                              {is_obstacle, obs_map}
                             end
     {robot, obs_map, is_obstacle}
   end
-  def update_obs_map(%ToyRobot.Position{x: x, y: y, facing: facing} = robot, obs_map, is_obstacle) do
-    obs_map= if is_obstacle==true do
-      obs_map=if facing==:east do
-                index=(@robot_map_y_atom_to_num[y])*4+x-5
-                obs_map=List.delete_at(obs_map,index)
-                obs_map=List.insert_at(obs_map,index,1)
-                obs_map
-              end
-      obs_map=if facing==:west do
-                index=(@robot_map_y_atom_to_num[y])*4+x-6
-                obs_map=List.delete_at(obs_map,index)
-                obs_map=List.insert_at(obs_map,index,1)
-                obs_map
-              end
-      obs_map=if facing==:north do
-                index=(@robot_map_y_atom_to_num[y])*5+x+14
-                obs_map=List.delete_at(obs_map,index)
-                obs_map=List.insert_at(obs_map,index,1)
-                obs_map
-              end
-      obs_map=if facing==:south do
-                index=(@robot_map_y_atom_to_num[y])*5+x+9
-                obs_map=List.delete_at(obs_map,index)
-                obs_map=List.insert_at(obs_map,index,1)
-                obs_map
-              end
-    end
+  def update_obs_map(%ToyRobot.Position{x: x, y: y, facing: facing} = _robot, obs_map, is_obstacle) do
+    obs_map=if is_obstacle==true do
+              obs_map=if facing==:east do
+                        index=(@robot_map_y_atom_to_num[y])*4+x-5
+                        {_,obs_map}=List.pop_at(obs_map,index)
+                        obs_map=List.insert_at(obs_map,index,1)
+                        obs_map
+                      else
+                        obs_map
+                      end
+              obs_map=if facing==:west do
+                        index=(@robot_map_y_atom_to_num[y])*4+x-6
+                        {_,obs_map}=List.pop_at(obs_map,index)
+                        obs_map=List.insert_at(obs_map,index,1)
+                        obs_map
+                      else
+                        obs_map
+                      end
+              obs_map=if facing==:north do
+                        index=(@robot_map_y_atom_to_num[y])*5+x+14
+                        {_,obs_map}=List.pop_at(obs_map,index)
+                        obs_map=List.insert_at(obs_map,index,1)
+                        obs_map
+                      else
+                        obs_map
+                      end
+              obs_map=if facing==:south do
+                        index=(@robot_map_y_atom_to_num[y])*5+x+9
+                        {_,obs_map}=List.pop_at(obs_map,index)
+                        obs_map=List.insert_at(obs_map,index,1)
+                        obs_map
+                      else
+                        obs_map
+                      end
+              obs_map
+            else
+              obs_map
+            end
     obs_map
   end
   def make_obsmap(obs_map, index) when index==40 do
